59ea32dca6a11b5e0f783fa8fe75391ac2ba8615
OlgaNovik1 <110986216+OlgaNovik1@users.noreply.github.com>
2023-04-23 13:34:49 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar/Navbar';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
